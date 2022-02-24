98d5e2eda1a319209f429ac9f22f375115a36891
Li Huang Wren <89958717+hl5284925@users.noreply.github.com>
2022-02-24 17:17:11 -0600

    Update InformationNotFoundException.java

--- src/main/java/com/example/musicloudbackend/exceptions/InformationNotFoundException.java
+++ src/main/java/com/example/musicloudbackend/exceptions/InformationNotFoundException.java
@@ -1,11 +1,9 @@
 package com.example.musicloudbackend.exceptions;
 
 
-
-
-
 public class InformationNotFoundException extends RuntimeException{
 
     public InformationNotFoundException(String message){
         super(message);
-    }}
+    }
+}
